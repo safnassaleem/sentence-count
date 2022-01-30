84c0845700ca41310ec00636ae51b00ddb363628
safnas saleem <safnas@hamon.in>
2022-01-30 23:33:31 +0530

    add test for sentence count

--- sample.py
+++ sample.py
@@ -4,3 +4,11 @@ import string
 def wordcount(string):
     return len(string.split(' '))
 print(wordcount("i am programer"))
+
+def sentence_count(string):
+    dot=string.count(".")
+    questionmark=string.count("?")
+    comma=string.count(",")
+    count=dot+questionmark+comma
+    return(count)
+print(sentence_count("iam a officer,working at? its good,to see."))
--- test_sample.py
+++ test_sample.py
@@ -1,3 +1,6 @@
 import sample
 def test_wordcount():
     assert sample.wordcount("i am programmer")==3
+
+def sentence_count():
+    assert sample.sentence_count("iam a officer,working at? its good,to see.")==4
