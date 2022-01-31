634794135bb8a81c7f1a5cb2d55ce87d0ea16b2e
safnas saleem <safnas@hamon.in>
2022-01-31 10:47:27 +0530

    changed the comma into exclamatory

--- sample.py
+++ sample.py
@@ -1,14 +1,14 @@
-import string
+# import string
 
 
-def wordcount(string):
-    return len(string.split(' '))
-print(wordcount("i am programer"))
+# def wordcount(string):
+#     return len(string.split(' '))
+# print(wordcount("i am programer"))
 
 def sentence_count(string):
     dot=string.count(".")
     questionmark=string.count("?")
-    comma=string.count(",")
-    count=dot+questionmark+comma
+    exclamatory=string.count("!")
+    count=dot+questionmark+exclamatory
     return(count)
-print(sentence_count("iam a officer,working at? its good,to see."))
+print(sentence_count("wow! i cant't believe we wom! it was nice.is there is any issue?"))
--- test_sample.py
+++ test_sample.py
@@ -3,4 +3,4 @@ def test_wordcount():
     assert sample.wordcount("i am programmer")==3
 
 def sentence_count():
-    assert sample.sentence_count("iam a officer,working at? its good,to see.")==4
+    assert sample.sentence_count("wow! i cant't believe we wom! it was nice.is there is any issue?")==4
